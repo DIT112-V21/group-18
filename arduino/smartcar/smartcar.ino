eb24b69df60944d6bad75f1331ca77bf10f5cadd
Fabian <fabianschlemmer@gmail.com>
2021-04-09 17:16:39 +0200

    Avoid obstacles by stopping and turning around

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,47 @@
 #include <Smartcar.h>
 
+ArduinoRuntime arduinoRuntime;
+SR04 front(arduinoRuntime, 6, 7, 200);
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+SimpleCar car(control);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(280);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  handleInput();
+  preventCrash();
+}
+
+void handleInput() {
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    if (input.startsWith("m")) {
+      int throttle = input.substring(1).toInt();
+      car.setSpeed(throttle);
+    }
+    else if (input.startsWith("t")) {
+      int deg = input.substring(1).toInt();
+      car.setAngle(deg);
+    }
+    else if (input.startsWith("r")) {
+      control.overrideMotorSpeed(100, -100);
+    }
+  }
+}
+
+void preventCrash() {
+  unsigned int fDistance = front.getDistance();
+  if (fDistance > 0) {
+    car.setSpeed(0);
+    delay(500);
+    control.overrideMotorSpeed(100, -100);
+    delay(1250);
+    car.setSpeed(0);
+  }
 }
