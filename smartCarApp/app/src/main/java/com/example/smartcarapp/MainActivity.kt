dbb04dabdc2a379bba47ab8469feb66c21432011
Trenavix <31814518+Trenavix@users.noreply.github.com>
2021-05-14 03:55:10 -0700

    Navigation Menu Added
    +Added hamburger button
    +Styled the navigation header
    +Added linear layout swapping for main activity
    +Renamed login activity
    +Cleaned up a bunch of code

--- smartCarApp/app/src/main/java/com/example/smartcarapp/MainActivity.kt
+++ smartCarApp/app/src/main/java/com/example/smartcarapp/MainActivity.kt
@@ -1,73 +1,72 @@
 package com.example.smartcarapp
-
 import android.content.Context
-import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import com.google.android.material.textfield.TextInputLayout
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_control.*
+import kotlinx.android.synthetic.main.content_main.*
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
+import kotlin.math.cos
+import kotlin.math.sin
 
-//Main Activity
-//Potential todo: create class for MQTT client to function between multiple activities
-//Temporary solution: MQTT client initialised on each activity (for now only active on ControlActivity)
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val connectButton = findViewById<Button>(R.id.testButton)
-        val publishMsgButton = findViewById<Button>(R.id.publishMsgButton)
-        val publishMsgText = findViewById<EditText>(R.id.publishMsgText)
-
-        val domainInput = findViewById<EditText>(R.id.domainInput)
-        connectButton.setOnClickListener {
-            val intent = Intent(this, ControlActivity::class.java)
-            intent.putExtra("domainInput", domainInput.text.toString())
-            startActivity(intent)
+        setContentView(com.example.smartcarapp.R.layout.activity_control)
+        connect(this)
+        setSupportActionBar(main_toolbar)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout,main_toolbar,R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        nav_menu.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.disconnect_item -> disconnect()
+                //Todo: create "layouts" for different items: Manual control, camera, mapview
+                R.id.manual_control_item -> {
+                    manual_control_layout.visibility = View.VISIBLE
+                    closeDrawer()
+                }
+            }
+            true
         }
-        publishMsgButton.setOnClickListener {
-            //val newMsg = publishMsgText.text.toString()
-            //publish("smartcar/", newMsg)
-            //publishMsgText.setText("")
+        joyStick.setOnMoveListener { angle, strength ->
+            //Todo: Reimplement this function with the .ino function to natively support the magnitude/angle rather than convert back and forth
+            val angleRadians = -angle.toFloat() * Math.PI / 180
+            val length = strength.toFloat() / 100 //change from 0 to 100, to 0 to 1
+            val analogX = cos(angleRadians)*length
+            val analogY = -sin(angleRadians)*length
+            publish("smartcar/analog/",  analogX.toString() + "," + analogY.toString())
         }
-        //Old debug screen with manual MQTT message sending:
-        /*connectButton.setOnClickListener {
-            connect(this);
-        }
-        publishMsgButton.setOnClickListener {
-            val newMsg = publishMsgText.text.toString()
-            publish("smartcar/", newMsg)
-            publishMsgText.setText("")
-        }*/
     }
+
     private lateinit var mqttClient: MqttAndroidClient
     // TAG
     companion object {
         const val TAG = "AndroidMqttClient"
     }
 
-
     fun connect(context: Context) { //Connect to domain and subscribe to topic automatically
-        val domainInput = findViewById<EditText>(R.id.domainInput)
-        val messagesBox = findViewById<TextView>(R.id.messagesBox)
-        val serverURI = "tcp://"+domainInput.text.toString()+":1883" //Change url to tcp and use port 1883 automatically for now
+        val domain=intent.getStringExtra("domainInput")
+        val serverURI = "tcp://"+domain.toString()+":1883" //Change url to tcp and use port 1883 automatically for now
         mqttClient = MqttAndroidClient(context, serverURI, "kotlin_client")
         mqttClient.setCallback(object : MqttCallback {
             override fun messageArrived(topic: String?, message: MqttMessage?) {
-                Log.d(TAG, "Receive message: ${message.toString()} from topic: $topic") //logcat logging
-                messagesBox.text = messagesBox.text.toString() + "Receive message: ${message.toString()} from topic: $topic\n"
+                Log.d(MainActivity.TAG, "Received a message from topic: $topic") //logcat logging
             }
 
             override fun connectionLost(cause: Throwable?) {
-                Log.d(TAG, "Connection lost ${cause.toString()}")
+                Log.d(MainActivity.TAG, "Connection lost ${cause.toString()}")
+                finish() //Return to login activity
             }
 
             override fun deliveryComplete(token: IMqttDeliveryToken?) {
@@ -78,16 +77,13 @@ class MainActivity : AppCompatActivity() {
         try {
             mqttClient.connect(options, null, object : IMqttActionListener {
                 override fun onSuccess(asyncActionToken: IMqttToken?) {
-                    Log.d(TAG, "Connection success") //logcat logging
-                    messagesBox.text = "Connected to domain: " + domainInput.text + "\n"
-                    findViewById<TextView>(R.id.publishMsgText).setVisibility(View.VISIBLE) //Set the publish mqtt message controls to visible
-                    findViewById<TextView>(R.id.publishMsgButton).setVisibility(View.VISIBLE)
+                    Log.d(MainActivity.TAG, "Connection success") //logcat logging
                     subscribe("smartcar/#", 1)
                 }
 
                 override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                    Log.d(TAG, "Connection failure") //logcat logging
-                    messagesBox.text = "Failed to connect to " + domainInput.text+"\n"
+                    Log.d(MainActivity.TAG, "Connection failure") //logcat logging
+                    finish() //return to login activity
                 }
             })
         } catch (e: MqttException) {
@@ -95,17 +91,33 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    fun disconnect() {
+        try {
+            mqttClient.disconnect(null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "Disconnected")
+                    finish()
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(TAG, "Failed to disconnect")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
     fun subscribe(topic: String, qos: Int = 1) {
         try {
             mqttClient.subscribe(topic, qos, null, object : IMqttActionListener {
                 override fun onSuccess(asyncActionToken: IMqttToken?) {
-                    Log.d(TAG, "Subscribed to $topic")
-                    val messagesBox = findViewById<TextView>(R.id.messagesBox)
-                    messagesBox.text = messagesBox.text.toString() + "Subscribed to $topic\n"
+                    Log.d(MainActivity.TAG, "Subscribed to $topic")
                 }
 
                 override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                    Log.d(TAG, "Failed to subscribe $topic")
+                    Log.d(MainActivity.TAG, "Failed to subscribe $topic")
                 }
             })
         } catch (e: MqttException) {
@@ -120,15 +132,21 @@ class MainActivity : AppCompatActivity() {
             message.isRetained = retained
             mqttClient.publish(topic, message, null, object : IMqttActionListener {
                 override fun onSuccess(asyncActionToken: IMqttToken?) {
-                    Log.d(TAG, "$msg published to $topic")
+                    Log.d(MainActivity.TAG, "$msg published to $topic")
                 }
 
                 override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                    Log.d(TAG, "Failed to publish $msg to $topic")
+                    Log.d(MainActivity.TAG, "Failed to publish $msg to $topic")
                 }
             })
         } catch (e: MqttException) {
             e.printStackTrace()
+        }
+    }
+
+    fun closeDrawer() { //Close main navigation drawer (hamburger button)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
         }
     }
 }
