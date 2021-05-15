6329fc34b144faac3f0a466af180e72d959bd862
alynnka <alynnka19@gmail.com>
2021-05-15 19:29:22 +0200

    Include manual movements in the vehicle

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -30,7 +30,7 @@ GP2D120 frontIR(arduinoRuntime, FRONT_IR_PIN_0);
 GP2D120 leftIR(arduinoRuntime,LEFT_IR_PIN_1);
 
 const int NON_VALID_MEASUREMENT = 0;
-boolean finishedCleaning = false;
+boolean finishedCleaning = true;
 
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
@@ -108,20 +108,26 @@ void joystick(float x, float y) {
   // pythagoras for magnitude of the vector
   float magnitude = sqrt(x*x + y*y);
   // dot product of forward vector [0, 1] and [x, y] divided by the magnitudes of the two vectors, then take arccos for the angle and convert from radians to degrees
-  double degree = (acos(y / magnitude)) * 180 / PI;
+  double degree = (acos(-y / magnitude)) * 180 / PI - 90;
 
   // figure out if we're going forward or backward, also amplify value from a normalized -1 to 1 input
   if (y >= 0)
-    magnitude = magnitude * 100;
-  else 
     magnitude = magnitude * -100;
+  else 
+    magnitude = magnitude * 100;
 
   // check if we're going left
-  if (x < 0)
-    degree = degree * -1;
+  if (x < 0){
+    degree = abs(degree) * -1;
+  }
+  else {
+    degree = abs(degree);
+  }
 
   car.setSpeed(magnitude);
-  car.setAngle((int)degree);
+  if(degree > -20 && degree < 20){
+    car.setAngle((int)degree);
+  }
   delay(10);
 }
 
